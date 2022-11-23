e5e441e4f6736207de97ed7c877985cec44065e3
unknown <ferdyfianaziizul@gmail.com>
2022-11-23 19:16:25 +0700

    ok

--- src/Reducer/Action.js
+++ src/Reducer/Action.js
@@ -2,7 +2,7 @@ import { message } from "antd";
 import axios from "axios";
 import _ from "lodash";
 
-export const BASE_URL = "http://localhost/jamtangankuid_server";
+export const BASE_URL = "http://192.168.43.92/jamtangankuid_server";
 
 const err_handle = (err) => {
   // console.log(err);
